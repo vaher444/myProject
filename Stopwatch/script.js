e4f08d3df0188f53a28c25bbb57bd1bd038a4609
Vishal Aher <ahervishal333@gmail.com>
2023-08-06 21:33:23 +0530

    Add files via upload

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -10,26 +10,26 @@ function startWatch() {
 }
 
 function stopWatch() {
-    clearInterval(interval); // stop the interval
-    OnPausedTime = new Date().getTime() - stTime; // calculate elapsed paused time
-    interval = null; // reset the interval variable
+  clearInterval(interval); // stop the interval
+  OnPausedTime = new Date().getTime() - stTime; // calculate elapsed paused time
+  interval = null; // reset the interval variable
 }
 
 function resetWatch() {
-    stopWatch(); // stop the interval
-    OnPausedTime = 0; // reset the elapsed paused time variable
-    document.getElementById("stopwatch").innerHTML = "00:00:00"; // reset the display
+  stopWatch(); // stop the interval
+  OnPausedTime = 0; // reset the elapsed paused time variable
+  document.getElementById("stopwatch").innerHTML = "00:00:00"; // reset the display
 }
-
+  
 function updateStopWatch() {
-    var currentTime = new Date().getTime(); // get current time in milliseconds
-    var elapsedTime = currentTime - stTime; // calculate elapsed time in milliseconds
-    var seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
-    var minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
-    var hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
-    var displayTime = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds); // format display time
-    document.getElementById("stopwatch").innerHTML = displayTime; // update the display
-  }
+  var currentTime = new Date().getTime(); // get current time in milliseconds
+  var elapsedTime = currentTime - stTime; // calculate elapsed time in milliseconds
+  var seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
+  var minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
+  var hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
+  var displayTime = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds); // format display time
+  document.getElementById("stopwatch").innerHTML = displayTime; // update the display
+}
   
 function addZero(number) {
   // adding a leading zero if the number is less than 10
